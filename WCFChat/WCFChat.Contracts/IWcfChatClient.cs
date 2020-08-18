819890752bd4cae25e96df4bced349f0f9356e93
André <ruland@outlook.de>
2020-08-18 10:53:17 +0200

    WCFChat Contracts

---
+++ WCFChat/WCFChat.Contracts/IWcfChatClient.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.ServiceModel;
+
+namespace WCFChat.Contracts
+{
+    [ServiceContract]
+    public interface IWcfChatClient
+    {
+        [OperationContract(IsOneWay = true)]
+        void LoginResponse(bool loginOk, string msg);
+
+        [OperationContract(IsOneWay = true)]
+        void LogoutResponse(bool logoutOk, string msg);
+
+        [OperationContract(IsOneWay = true)]
+        void ShowText(string msg);
+
+        [OperationContract(IsOneWay = true)]
+        void ShowImage(Stream image);
+        
+        [OperationContract(IsOneWay = true)]
+        void ShowUserlist(IEnumerable<string> users);
+    }
+}
---
+++ WCFChat/WCFChat.Contracts/IWcfChatServer.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.ServiceModel;
+
+namespace WCFChat.Contracts
+{
+    [ServiceContract(CallbackContract = typeof(IWcfChatClient))]
+    public interface IWcfChatServer
+    {
+        [OperationContract(IsOneWay = true)]
+        void Login(string user);
+
+        [OperationContract(IsOneWay = true)]
+        void Logout();
+
+        [OperationContract(IsOneWay = true)]
+        void Whisper(string to, string msg);
+
+        [OperationContract(IsOneWay = true)]
+        void SendText(string msg);
+
+        [OperationContract(IsOneWay = true)]
+        void SendImage(Stream image);
+    }
+}
